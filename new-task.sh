3cdabe9b8bee0ed4cfd079abe3d1a5048d107b44
Aleksandr Kolesavin <a.v.kolesavin@gmail.com>
2021-11-21 21:07:06 +0300

    refactor: update 'new-task' script

--- new-task.sh
+++ new-task.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+taskFile="$2.js"
+testFile="$2.test.js"
+taskTemplate="export default function taskName() {}"
+testTemplate="
+import taskFunction from './$2.js';
+\n\ndescribe('Task $2', () => {
+\n\tit('Should ...', () => {});
+\n});
+"
+
 if [ -n $1 ] && [ -n $2 ]; then
-    cd $1 && touch "$2.js" "$2.test.js"
+    mkdir $1 && cd $1 && touch $taskFile $testFile &&
+    echo -e $taskTemplate >> $taskFile &&
+    echo -e $testTemplate >> $testFile
 fi
