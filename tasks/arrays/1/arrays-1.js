3de8903b4f1bd6543c656d9fed483b08e0ac6a32
Aleksandr Kolesavin <a.v.kolesavin@gmail.com>
2021-11-28 15:29:09 +0300

    refactor: update tests for arrays 1st task

--- tasks/arrays/1/arrays-1.js
+++ tasks/arrays/1/arrays-1.js
@@ -2,6 +2,8 @@
  * Function takes array of numbers (negative and positive)
  * and should turn all negative numbers into positive
  * and return this array
+ * 
+ * REMARK: passed array should not be mutated
  */
 export default function makeArrayPositive(array) {
     return array;
--- tasks/arrays/1/arrays-1.test.js
+++ tasks/arrays/1/arrays-1.test.js
@@ -4,8 +4,14 @@ describe('Task arrays-1', () => {
     it.each([
         [[], []],
         [[1, 2, 3], [1, 2, 3]],
+    ])('Should for %j return %j', (array, expected) => {
+        expect(taskFunction(array)).toEqual(expected);
+    });
+
+    it.each([
         [[0, -1, 2, -3, 4], [0, 1, 2, 3, 4]]
     ])('Should for %j return %j', (array, expected) => {
         expect(taskFunction(array)).toEqual(expected);
+        expect(array).not.toEqual(expected);
     });
 });
